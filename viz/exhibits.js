9e6eab6f48894d2e9ab7f6a8e985f1bc950b472a
Ted Benson <edward.benson@gmail.com>
2012-02-23 12:51:54 -0500

    exhibits exhibit

---
+++ viz/exhibits.js
@@ -0,0 +1,278 @@
+{'items':[
+
+{
+  'label':'MIT Physics Faculty',
+  'url':'http://web.mit.edu/physics/people/faculty/index.html',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Directory'],
+  'itemType':['People'],
+  'siteType':['School']
+},
+{
+  'label':'Science Maps',
+  'url':'http://scimaps.org/maps/browse/',
+  'tld':'org',
+  'category':['Education'],
+  'purpose':['Curation'],
+  'itemType':['Maps'],
+  'siteType':['Organization'],
+},
+{
+  'label':'MIT Physics Classes - All',
+  'url':'http://web.mit.edu/physics/subjects/index.html',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['Classes'],
+  'siteType':['School']
+},
+{
+  'label':'MIT Physics Classes - Current',
+  'url':'http://web.mit.edu/physics/subjects/current.html',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['Classes'],
+  'siteType':['School']
+},
+{
+  'label':'MIT Physics Classses - First Year',
+  'url':'http://web.mit.edu/physics/subjects/firstyr.html',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['Classes'],
+  'siteType':['School']
+},
+{
+  'label':'Linked Data Publications',
+  'url':'http://www.linkedopendata.it/apps/exh/loc2.html',
+  'tld':'it',
+  'category':['Web Science'],
+  'purpose':['Curation'],
+  'itemType':['Publications'],
+  'siteType':['Organization']
+},
+{
+  'label':'Conflicts',
+  'url':'http://hiik.de/exhibit_09/',
+  'tld':'de',
+  'category':['Politics'],
+  'purpose':['Enumeration'],
+  'itemType':['Events', 'Conflicts'],
+  'siteType':['Organization']
+},
+{ 
+  'label':'City Classes',
+  'url':'http://www.staff.city.ac.uk/~sbbf539/bptcft/',
+  'tld':'uk',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['Classes'],
+  'siteType':['School']
+},
+{
+  'label':'Scimaps Atlas',
+  'url':'http://scimaps.org/atlas/'
+},
+{
+  'label':'Farmers Markets',
+  'url':'http://ww2.startribune.com/projects/farmersmarkets/',
+  'tld':'com',
+  'category':['Commercial'],
+  'purpose':['Curation'],
+  'itemType':['Events', 'Commercial Locations', 'Markets'],
+  'siteType':['News']
+},
+{
+  'label':'Kent Law Library Resources',
+  'url':'http://library.kentlaw.edu/eresources/index.htm',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Curation'],
+  'itemType':['Publication Sources', 'Resources'],
+  'siteType':['School', 'Library']
+},
+{
+  'label':'MIT Chemistry Faculty',
+  'url':'http://web.mit.edu/cheme/people/indexi.html',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Directory'],
+  'itemType':['People'],
+  'siteType':['School']
+},
+{
+  'label':'Publication List',
+  'url':'http://alex.smola.org/papers.html',
+  'tld':'org',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['Papers', 'Publications'],
+  'siteType':['Personal']
+},
+{
+  'label':'Columbia Law Library Resources',
+  'url':'http://library.law.columbia.edu/eresources/',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Curation'],
+  'itemType':['Publication Sources', 'Resources'],
+  'siteType':['School', 'Library']
+},
+{
+  'label':'Contaminated Municipalities',
+  'url':'http://5lineas.com/mapas/esp-municipios-mas-contaminados.html',
+  'tld':'com',
+  'category':['Environment'],
+  'purpose':['Curation'],
+  'itemType':['Environmental'],
+  'siteType':['Personal']
+},
+{
+  'label':'Composers',
+  'url':'http://www.musicedmagic.com/Exhibit/composers.html',
+  'tld':'com',
+  'category':['Music'],
+  'purpose':['Curation'],
+  'itemType':['People', 'Composers'],
+  'siteType':['Personal']
+},
+{
+  'label':'Stanford Business School Alumni Groups',
+  'url':'http://alumni.gsb.stanford.edu/about/alumni.html',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Enumeration', 'Aggregation'],
+  'itemType':['Groups'],
+  'siteType':['School']
+},
+{
+  'label':'Floorball Matches',
+  'url':'http://www.innebandycuper.se/',
+  'tld':'se',
+  'purpose':['Curation'],
+  'category':['Sports'],
+  'itemType':['Games', 'Events'],
+  'siteType':['Personal']
+},
+{
+  'label':'Personality Disorder Groups',
+  'url':'http://www.personalitydisorder.org.uk/includes/exhibit-small.php',
+  'tld':'uk',
+  'category':['Medical'],
+  'purpose':['Curation'],
+  'itemType':['Resources','Groups','Places'],
+  'siteType':['Organization']
+},
+{
+  'label':'MIT Nobel Prize Winners',
+  'url':'http://simile.mit.edu/exhibit/examples/nobelists/nobelists.html',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Curation'],
+  'itemTpe':['People'],
+  'siteType':['School']
+},
+{
+  'label':'MIT Physics Faculty',
+  'url':'http://web.mit.edu/physics/research/abcp/faculty.html',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Directory'],
+  'itemType':['People'],
+  'siteType':['School']
+},
+{
+  'label':'City School Classes',
+  'url':'http://semantic.city.ac.uk/pob/bptcft/',
+  'tld':'uk',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['Classes'],
+  'siteType':['School']
+},
+{
+  'label':'Accepted CVPR 2010 Conference Papers',
+  'url':'http://cvl.umiacs.umd.edu/conferences/cvpr2010/results/',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['Papers','Publications'],
+  'siteType':['School','Conference']
+},
+{
+  'label':'Education Policy and Data Center Resources',
+  'url':'http://www.epdc.org/policyanalysis/default.aspx',
+  'tld':'org',
+  'category':['Education'],
+  'purpose':['Curation'],
+  'itemType':['Publications','Resources'],
+  'siteType':['Organization']
+},
+{
+  'label':'Peyre Publications',
+  'url':'http://www.ceremade.dauphine.fr/~peyre/publications/',
+  'tld':'fr',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['Papers','Publications'],
+  'siteType':['Personal']
+},
+{
+  'label':'Pappalardo Fellows Directory',
+  'url':'http://web.mit.edu/physics/people/pappalardo/index.html',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['People'],
+  'siteType':['School']
+},
+{
+  'label':'Bay Area Foreclosures',
+  'url':'http://www.sfgate.com/maps/foreclosures/',
+  'tld':'com',
+  'category':['Real Estate','Finance'],
+  'purpose':['Enumeration'],
+  'itemType':['Foreclosures'],
+  'siteType':['News']
+},
+{
+  'label':'MIT Physics Administrators',
+  'url':'http://web.mit.edu/physics/people/admin/index.html',
+  'tld':'edu',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['People'],
+  'siteType':['School']
+},
+{
+  'label':'City Populations in Spain',
+  'url':'http://5lineas.com/mapas/esp-municipios-mas-50mil.html',
+  'tld':'com',
+  'category':['Government'],
+  'purposes':['Enumeration'],
+  'itemType':['Cities'],
+  'siteType':['Personal']
+},
+{
+  'label':'Beethoven Timeline',
+  'url':'http://www.musicedmagic.com/Exhibit/beethoventimeline.html',
+  'tld':'com',
+  'category':['Music'],
+  'purposes':['Curation'],
+  'itemType':['Events'],
+  'siteType':['Personal']
+},
+{
+  'label':'Projects',
+  'url':'http://www.plantsci.cam.ac.uk/grads/selecting.html',
+  'tld':'uk',
+  'category':['Education'],
+  'purpose':['Enumeration'],
+  'itemType':['Projects'],
+  'siteType':['School']
+}
+
+]}
